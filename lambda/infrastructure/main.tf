bfe77168666ede0412efbdeebfef73db54b11b42
Lee Korff-Korn <Leekk270@gmail.com>
2025-01-02 13:59:50 -0500

    try1

--- lambda/infrastructure/main.tf
+++ lambda/infrastructure/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket         = "sig-standardizer-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
