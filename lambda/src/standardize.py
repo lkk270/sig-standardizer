ceac71b5ef5b682cb3f836dd02c2a32f60f7c577
Lee Korff-Korn <Leekk270@gmail.com>
2025-01-03 16:16:10 -0500

    changed model

--- lambda/src/standardize.py
+++ lambda/src/standardize.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
 
         logger.info("Making request to OpenAI API")
         response = client.chat.completions.create(
-            model="chatgpt-4o-latest",
+            model="gpt-4o-2024-11-20",
             messages=[
                 {"role": "system",
                     "content": "Standardize the following signature block format."},
